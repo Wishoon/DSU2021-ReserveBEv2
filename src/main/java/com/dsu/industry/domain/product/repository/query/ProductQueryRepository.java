d48bfff7c9ca2620af9661a62e426573baacc923
wishoon <heonga2@gmail.com>
2021-08-28 23:44:57 +0900

    [Feat] Product 도메인 Query Repository 생성

---
+++ src/main/java/com/dsu/industry/domain/product/repository/query/ProductQueryRepository.java
@@ -0,0 +1,21 @@
+package com.dsu.industry.domain.product.repository.query;
+
+import com.dsu.industry.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ProductQueryRepository extends JpaRepository<Product, Long> {
+
+    @Query("select p from Product p" +
+            " join p.calendarList cd on cd.date between :checkIn and :checkOut" +
+            " where p.address.addr1_depth_nm = :city" +
+            " and p.category.name = :category")
+    List<Product> findProductByAvailable(@Param("category") String category,
+                                         @Param("city") String city,
+                                         @Param("checkIn") LocalDate checkIn,
+                                         @Param("checkOut") LocalDate checkOut);
+}
