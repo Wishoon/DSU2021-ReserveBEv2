8731f3e0cbaebe1fd90fd6165ab3e168394aaee8
WishooN <48710213+Wishoon@users.noreply.github.com>
2021-09-02 19:38:57 +0900

    [Feat] JWT Configuration을 binding하는 POJO 클래스 생성

--- src/main/java/com/dsu/industry/Dsu2021ReserveApplication.java
+++ src/main/java/com/dsu/industry/Dsu2021ReserveApplication.java
@@ -1,10 +1,12 @@
 package com.dsu.industry;
 
+import com.dsu.industry.global.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class Dsu2021ReserveApplication {
 
     public static void main(String[] args) {
