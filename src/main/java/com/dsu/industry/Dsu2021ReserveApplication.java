68c060d3b7b7a1acd2facf471f24e22f5a94555f
WishooN <48710213+Wishoon@users.noreply.github.com>
2021-09-02 20:14:58 +0900

    [Feat] @EnableJpaAuditing 설정

--- src/main/java/com/dsu/industry/Dsu2021ReserveApplication.java
+++ src/main/java/com/dsu/industry/Dsu2021ReserveApplication.java
@@ -4,8 +4,10 @@ import com.dsu.industry.global.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableConfigurationProperties(AppProperties.class)
 public class Dsu2021ReserveApplication {
 
--- src/main/java/com/dsu/industry/global/common/BaseEntity.java
+++ src/main/java/com/dsu/industry/global/common/BaseEntity.java
@@ -3,14 +3,17 @@ package com.dsu.industry.global.common;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
-public class BaseEntity {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
