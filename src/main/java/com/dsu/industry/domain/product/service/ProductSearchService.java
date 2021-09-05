8e62938f0da4c3dfa4268d0204e53b5a0e099918
WishooN <48710213+Wishoon@users.noreply.github.com>
2021-09-05 23:43:22 +0900

    [Feat] 상품에 대한 예약 가능 날자 조회 로직 추가

--- src/main/java/com/dsu/industry/domain/product/service/ProductSearchService.java
+++ src/main/java/com/dsu/industry/domain/product/service/ProductSearchService.java
@@ -1,8 +1,9 @@
 package com.dsu.industry.domain.product.service;
 
 import com.dsu.industry.domain.product.dto.ProductDto;
-import com.dsu.industry.domain.product.dto.ProductSearchDto;
+import com.dsu.industry.domain.product.entity.AvailableDate;
 import com.dsu.industry.domain.product.entity.Product;
+import com.dsu.industry.domain.product.repository.AvailableDateRepository;
 import com.dsu.industry.domain.product.repository.query.ProductQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,19 +16,31 @@ import java.util.stream.Collectors;
 public class ProductSearchService {
 
     private final ProductQueryRepository productQueryRepository;
+    private final AvailableDateRepository availableDateRepository;
 
     /**
      * 유저가 상품의 카테고리, 도시, 체크인, 체크아웃으로 정보 다중 조회
      * @param dto
      * @return
      */
-    public List<ProductDto.ProductInfoRes> product_searchList(ProductSearchDto.SearchReq dto) {
+    public List<ProductDto.ProductInfoRes> product_searchList(ProductDto.ProductSearchReq dto) {
 
         List<Product> products = productQueryRepository.findProductByAvailable(
                 dto.getCategory(), dto.getCity(), dto.getCheckIn(), dto.getCheckOut(), dto.getDateCnt());
 
         return products.stream()
                 .map(ProductDto.ProductInfoRes::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductDto.ProductAvailableRes> product_available_search(ProductDto.ProductAvailableReq req) {
+
+        List<AvailableDate> availableDates = availableDateRepository.findByProductAndDateGreaterThanEqual(
+                req.getProduct(), req.getToday()
+        );
+
+        return availableDates.stream()
+                .map(ProductDto.ProductAvailableRes::toDto)
                 .collect(Collectors.toList());
     }
 }
