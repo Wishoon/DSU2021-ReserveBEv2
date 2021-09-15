6b460f9633c98087ab60f5982f1ea77fd7ce08a2
WishooN <48710213+Wishoon@users.noreply.github.com>
2021-09-15 17:35:58 +0900

    [Revise] Repository 이름 변경 및 package 위치 변경

--- src/main/java/com/dsu/industry/domain/product/repository/query/ProductQueryRepository.java
+++ src/main/java/com/dsu/industry/domain/product/repository/query/ProductQueryRepository.java
@@ -1,43 +1,41 @@
 package com.dsu.industry.domain.product.repository.query;
 
+import com.dsu.industry.domain.product.dto.ProductDto;
 import com.dsu.industry.domain.product.entity.Product;
-import org.apache.tomcat.jni.Local;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import javax.persistence.EntityManager;
 import java.util.List;
-import java.util.Optional;
 
-public interface ProductQueryRepository extends JpaRepository<Product, Long> {
+@RequiredArgsConstructor
+@Repository
+public class ProductQueryRepository {
 
-    /* 상품 검색에서 예약 가능 여부 확인 로직*/
-    @Query("select p from Product p" +
-            " where p.id in (select cd.product.id from AvailableDate cd join cd.product p" +
-                            " where cd.date between :checkIn and :checkOut" +
-                            " group by p.id having count(p.id) = :count)" +
-            " and p.address.addr1_depth_nm = :city" +
-            " and p.category.name = :category")
-    List<Product> findProductByAvailableWithCategoryAndCity(
-            @Param("category") String category,
-            @Param("city") String city,
-            @Param("checkIn") LocalDate checkIn,
-            @Param("checkOut") LocalDate checkOut,
-            @Param("count") Long count);
+    private final EntityManager em;
+
+    // 아직 동적 쿼리 적용이 되어 있지 않음. -> QueryDSL 변경 예정
+    // distinct를 할 경우 1:N 구조의 중복 데이터를 제거해주기는 함
+    // 단, DB쿼리에서는 동일하게 나가기 떼문에 JPA 내에서 자체적으로 distinct를 해준다. (페이징 불가능)
+    public List<Product> findProductByAvailableDateWithCategoryAndCity(
+            ProductDto.ProductSearchReq dto) {
+
+        return em.createQuery(
+                "select p from Product p" +
+                        " join fetch p.category c" +
+                        " join fetch p.address a" +
+                        " where p.id in (select cd.product.id from AvailableDate cd join cd.product p" +
+                                        " where cd.date between :checkIn and :checkOut" +
+                                        " group by p.id having count(p.id) = :dateCnt)" +
+                        " and a.addr1_depth_nm = :city" +
+                        " and c.name = :category", Product.class)
+                .setParameter("category", dto.getCategory())
+                .setParameter("city", dto.getCity())
+                .setParameter("checkIn", dto.getCheckIn())
+                .setParameter("checkOut", dto.getCheckOut())
+                .setParameter("dateCnt", dto.getDateCnt())
+                .getResultList();
+    }
 
 
-    /* 체크인 체크아웃 조건에 의한 상품 예약 가능 여부 확인 로직*/
-    /*
-    @Query("select p from Product p" +
-            " where p = :product" +
-            " and :product = (select cd.product from AvailableDate cd join cd.product p" +
-                            " where cd.date between :checkIn and :checkOut" +
-                            " and cd.product =: product" +
-                            " group by p.id having count(p.id) = :count)")
-    Optional<Product> findProductByAvailable(
-            @Param("product") Product product,
-            @Param("checkIn") LocalDate checkIn,
-            @Param("checkOut") LocalDate checkOut,
-            @Param("count") Long count); */
 }
