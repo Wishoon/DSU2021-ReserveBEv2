d70b790af7de458b421510ea2a0c4d1f18106992
WishooN <48710213+Wishoon@users.noreply.github.com>
2021-10-02 02:22:32 +0900

    [Feat] profile log 추가

--- src/main/java/com/dsu/industry/global/profile/ProfileController.java
+++ src/main/java/com/dsu/industry/global/profile/ProfileController.java
@@ -1,6 +1,7 @@
 package com.dsu.industry.global.profile;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
@@ -19,6 +21,7 @@ public class ProfileController {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("local", "real1", "real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+        log.info("Current Profile : " + defaultProfile);
         return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
     }
 }
